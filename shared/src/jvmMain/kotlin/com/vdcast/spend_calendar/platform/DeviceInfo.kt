083b7fd4d09128ae26033b54304400adf45fb4d3
VD <vdfree448@protonmail.com>
2024-04-09 20:32:41 +0300

    adding expect actual 1

---
+++ shared/src/jvmMain/kotlin/com/vdcast/spend_calendar/platform/DeviceInfo.kt
@@ -0,0 +1,22 @@
+package com.vdcast.spend_calendar.platform
+
+actual class DeviceInfo {
+    actual val osName: String
+        get() = TODO("Not implemented yet")
+    actual val osVersion: String
+        get() = TODO("Not implemented yet")
+    actual val model: String
+        get() = TODO("Not implemented yet")
+    actual val cpu: String
+        get() = TODO("Not implemented yet")
+    actual val screenWidth: Int
+        get() = TODO("Not implemented yet")
+    actual val screenHeight: Int
+        get() = TODO("Not implemented yet")
+    actual val screenDestiny: Int
+        get() = TODO("Not implemented yet")
+
+    actual fun getSummary(): String {
+        TODO("Not implemented yet")
+    }
+}
