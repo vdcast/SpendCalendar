8e9e52b5a1f163a802437f7fb2667e4261db0913
vdcast <vdfree448@protonmail.com>
2024-04-09 18:42:27 +0300

    added ios from mac

---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by vdcast on 09.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
