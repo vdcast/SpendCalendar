0ffd2e61ea8d25afc7530d2241b66f633cff9b1a
vdcast <vdfree448@protonmail.com>
2024-04-10 17:52:48 +0300

    lesson 9 processing from mac

--- shared/src/iosMain/kotlin/com/vdcast/spend_calendar/RootView.kt
+++ shared/src/iosMain/kotlin/com/vdcast/spend_calendar/RootView.kt
@@ -1,9 +1,10 @@
 package com.vdcast.spend_calendar
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.vdcast.spend_calendar.root.RootViewModel
 import com.vdcast.spend_calendar.root.compose.RootScreen
 
 fun MainViewController() = ComposeUIViewController {
 //    SayHelloFromCommon()
-    RootScreen()
+    RootScreen(RootViewModel())
 }
