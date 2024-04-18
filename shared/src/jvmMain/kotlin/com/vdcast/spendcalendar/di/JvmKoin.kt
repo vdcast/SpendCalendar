bdc84440cabbc4b9507155a8207a3071ccbf2150
VD <vdfree448@protonmail.com>
2024-04-18 13:49:07 +0300

    added check if exists AppDb

--- shared/src/jvmMain/kotlin/com/vdcast/spendcalendar/di/JvmKoin.kt
+++ shared/src/jvmMain/kotlin/com/vdcast/spendcalendar/di/JvmKoin.kt
@@ -7,14 +7,28 @@ import com.russhwolf.settings.Settings
 import com.vdcast.spendcalendar.db.AppDb
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import java.io.File
 import java.util.prefs.Preferences
 
 actual val platformModule: Module = module {
     single<Settings> { PreferencesSettings(Preferences.userRoot()) }
     single<SqlDriver> {
-//        val driver = JdbcSqliteDriver("jdbc:sqlite:AppDb.db")
-        val driver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
-        AppDb.Schema.create(driver)
+
+////        val driver = JdbcSqliteDriver("jdbc:sqlite:AppDb.db")
+//        val driver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+//        AppDb.Schema.create(driver)
+//        driver
+
+        val dbFilePath = "AppDb.db"
+        val dbFile = File(dbFilePath)
+        val isNewDatabase = !dbFile.exists()
+
+        val driver = JdbcSqliteDriver("jdbc:sqlite:$dbFilePath")
+
+        if (isNewDatabase) {
+            AppDb.Schema.create(driver) // Створення схеми, якщо це нова база
+        }
+
         driver
     }
 }
